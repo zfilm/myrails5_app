a071fa771e927f9bbc83ab9dfc255501096324c0
zfilm <morten.foyn.boe@gmail.com>
2016-12-16 12:15:45 +0100

    Changed Hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: 'Hello'
+    render html: 'Hello your world'
   end
 end
